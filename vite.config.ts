7bbffe1b1e78f80229336f535c0c9cf124c41257
jeeva-g <jeevajeeva060402@gmail.com>
2025-06-10 08:23:09 +0530

    Prepare for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/jeeva_portfolio/",
   server: {
     host: "::",
     port: 8080,
