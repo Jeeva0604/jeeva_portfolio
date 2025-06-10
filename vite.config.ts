d090d85e79c1f673be63db79b82c520a6d704e01
jeeva-g <jeevajeeva060402@gmail.com>
2025-06-10 08:06:40 +0530

    Update homepage URL in package.json and fix deploy script path in package.json; adjust Vite config plugins

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,12 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/my_portfolio/",
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
